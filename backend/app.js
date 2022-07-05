20c7757c3192132f22c3c62c90fc4cb4ce47a6a2
Mikhail Tsoy <mixail0071@yandex.ru>
2022-07-05 17:44:55 +0200

    fix: cors middleware

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
-app.use(cors);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.use(cors);
 app.post(
   '/signin',
   celebrate({
