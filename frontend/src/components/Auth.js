650e16c24ba4186fb0e7520eb0e56302bad713b9
Mikhail Tsoy <mixail0071@yandex.ru>
2022-07-06 12:29:41 +0200

    fix cors

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://auth.nomoreparties.co';
+const BASE_URL = 'https://mtsoy.numberone.nomoredomains.sbs';
 
 const _checkResponse = (url, options = {}) => {
   return fetch(url, options)
