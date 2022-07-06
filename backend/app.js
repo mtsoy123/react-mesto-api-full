332064f541aad1b268b931a37a0aa334c34259ca
Mikhail Tsoy <mixail0071@yandex.ru>
2022-07-06 22:57:07 +0200

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { PORT = 3000 } = process.env;
 require('dotenv').config();
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/fullapi');
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(cors);
 
