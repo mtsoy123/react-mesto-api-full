30d872e27ff933c35c481aa481be241457c635c2
Mikhail Tsoy <mixail0071@yandex.ru>
2022-07-06 23:02:34 +0200

    fix cors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,23 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedErr = require('../utils/errors/UnauthorizedErr');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw new UnauthorizedErr('Пользователь не авторизован');
+    throw new UnauthorizedErr('111Пользователь не авторизован');
   }
 
   let payload;
 
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+    );
   } catch (err) {
-    throw new UnauthorizedErr('Пользователь не авторизован');
+    throw new UnauthorizedErr('222Пользователь не авторизован');
   }
 
   req.user = payload;
