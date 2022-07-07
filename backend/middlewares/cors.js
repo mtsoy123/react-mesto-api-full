247517b03d278bb5267ba184770267e8740b1f46
Mikhail Tsoy <mixail0071@yandex.ru>
2022-07-07 12:49:14 +0200

    add credentials in fetch

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -13,12 +13,12 @@ module.exports = function (req, res, next) {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', 'https://mtsoy.numberone.nomoredomains.sbs');
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
     return res.status(200).end();
   }
 
